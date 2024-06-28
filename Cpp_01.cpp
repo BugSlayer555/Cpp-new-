4693c48e66cb7047de9c193457a7ea3117d6a68f
Aditya Vr <adityaverma4768@gmail.com>
2024-06-28 23:52:23 +0530

    Cpp

---
+++ Cpp_01.cpp
@@ -0,0 +1,63 @@
+//______Output_____
+/*#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Hello";
+}*/
+
+
+
+
+//_____Next Line_____
+/*#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Hello"<<endl;
+    cout<<"Aditya"<<"\n";
+    cout<<"Verma";
+}*/
+
+
+
+
+//________Variables as Containers________
+/*#include<iostream>
+using namespace std;
+int main(){
+    int x;
+    x = 5;
+    cout<<x;
+}*/
+
+
+
+
+//________Updation of varaible__________
+/*#include<iostream>
+using namespace std;
+int main(){
+    int x;
+    x = 5;
+    cout<<x<<endl;
+    x = 7;
+    cout<<x<<endl;
+    x = 8;
+    cout<<x;
+}*/
+
+
+
+//_________Arithmetic Operations__________
+/*#include<iostream>
+using namespace std;
+int main(){
+    int First;
+    First = 5;
+    int Second;
+    Second = 10;
+    cout<<First + Second<<endl;
+    cout<<First * Second<<endl;
+    cout<<Second / First<<endl;
+    cout<<Second - First<<endl; 
+    cout<<Second % First;
+}*/
