436a527ddba778a9776dee177fc780e444108594
Aditya Vr <adityaverma4768@gmail.com>
2024-06-30 10:10:51 +0530

    Cpp

---
+++ Cpp_02.cpp
@@ -0,0 +1,43 @@
+//_________Input & Output__________
+/*#include<iostream>
+using namespace std;
+int main(){
+    int first;
+    int second;
+    cout<<"Enter first number: ";//For printing something or we can say that to give an output
+    cin>>first;//For taking value from user
+    cout<<"Enter second number: ";
+    cin>>second;
+    cout<<"First number is: "<<first<<endl;
+    cout<<"Second number is: "<<second;
+}*/
+
+
+
+
+
+//________________Input & Output for Strings_________________
+/*#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    string str1;
+    cout<<"Enter your name: ";
+    cin>>str1;
+    cout<<"candidate name is: "<<str1;
+}*/
+
+
+
+
+
+
+//_______________Float________________
+#include<iostream>
+using namespace std;
+int main(){
+    float x;
+    cout<<"Enter Float Digit: ";
+    cin>>x;
+    cout<<x;
+}
